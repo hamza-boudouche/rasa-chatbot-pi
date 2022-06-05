d99e576504ce66296fb604f60ac66ffa3ea0c973
wissalakki <85134410+wissalakki@users.noreply.github.com>
2022-06-05 13:12:14 +0100

    adding the google calendar story and the update_task
    I added the update_task story and the confirmation actions to all the stories and I have made the changes to the google calendar stories

--- actions/actions.py
+++ actions/actions.py
@@ -18,9 +18,49 @@ class ActionConfirmAddEvent(Action):
         dispatcher.utter_message(
             text="you are about to start the procedure to add an event, do you want to continue?")
 
+class ActionConfirmAddTask(Action):
+
+    def name(self) -> Text:
+        return "action_confirm_add_task"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("confirm adding task")
+
         dispatcher.utter_message(
-            json_message={"requestForm": True}, text="hello")
-        return []
+            text="you are about to start the procedure to add a task, do you want to continue?")
+
+class ActionConfirmRemoveTask(Action):
+
+    def name(self) -> Text:
+        return "action_confirm_remove_task"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("confirm removing task")
+
+        dispatcher.utter_message(
+            text="you are about to start the procedure to remove a task, do you want to continue?")
+
+class ActionConfirmUpdateTask(Action):
+
+    def name(self) -> Text:
+        return "action_confirm_update_task"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("confirm updating task")
+
+        dispatcher.utter_message(
+            text="you are about to start the procedure to update a task, do you want to continue?")
+
+class ActionConfirmMoveTask(Action):
+
+    def name(self) -> Text:
+        return "action_confirm_move_task"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("confirm moving task")
+
+        dispatcher.utter_message(
+            text="you are about to start the procedure to move a task, do you want to continue?")
 
 
 class ActionAddEvent(Action):
@@ -31,7 +71,7 @@ class ActionAddEvent(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         print("adding event")
         dispatcher.utter_message(text="adding event")
-        dispatcher.utter_message(json_message={"requestForm": True})
+        dispatcher.utter_message(json_message={"formtype": "add_event"})
         return []
 
 class ActionAddTask(Action):
@@ -63,6 +103,16 @@ class ActionMoveTask(Action):
         print("moving task")
         dispatcher.utter_message(text="moving task")
         dispatcher.utter_message(json_message={"formtype": "move_task"})
+        return [] 
+
+class ActionUpdateTask(Action):
+    def name(self) -> Text:
+        return "action_update_task"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("update task")
+        dispatcher.utter_message(text="updating task")
+        dispatcher.utter_message(json_message={"formtype": "update_task"})
         return [] 
 
 class ActionResetAllSlots(Action):
